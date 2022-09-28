c728054c89dc762814f2210f31edd6577c2a659a
VoravichS <vsilapac@gmail.com>
2022-09-28 13:26:51 -0400

    merge conflict resolutions

--- client/src/components/pages/CharCust.js
+++ client/src/components/pages/CharCust.js
@@ -1,8 +1,23 @@
+const charSprites = [
+  { 
+    link: 'https://i.imgur.com/AOjR8o4.png',
+    name: 'warrior female 1'
+  },
+  { 
+    link: 'https://i.imgur.com/Hwzy38K.png',
+    name: 'legend of zelda link'
+  }
+];
+
 const CharCust = () => {
 
   return (
     <div>
-      <h1 className='text-5xl mb-12 text-yellow-100 text-center'>Character Customization</h1>
+      <h1 className='text-5xl mb-12 text-yellow-100 text-center'>Character Customization</h1>         
+      {charSprites.map((char) =>
+        <img className='w-1/4' src={char.link} alt={char.name} />
+      )}
+      
       <form className="text-center">
       <input className= 'text-center' placeholder="enter name"></input>
       </form>
