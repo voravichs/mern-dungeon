70e33af80103d221817b5b2f3e0b688ac65f4528
idakukimiya <kukimiya.ida@gmail.com>
2022-09-28 13:29:17 -0400

    fix merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:src/App.js
-import DugeonContainer from './components/DugeonContainer';
-=======
-import DungeonContainer from './components/DungeonContainer';
->>>>>>> 01e423bdf0bdd04ab698a250f551182b06eb5675:client/src/App.js
+import DugeonContainer from './components/DungeonContainer';
 import { Helmet } from 'react-helmet';
 
 
@@ -12,11 +8,9 @@ export default function App() {
       <Helmet>
         <script src="https://cdn.tailwindcss.com"></script>
       </Helmet>
-<<<<<<< HEAD:src/App.js
+
       <DugeonContainer />
-=======
-      <DungeonContainer />
->>>>>>> 01e423bdf0bdd04ab698a250f551182b06eb5675:client/src/App.js
+
     </div>
   )
 }
