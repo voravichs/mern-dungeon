873925f6569a554c5e5bc28865f13aac394b3d28
Eric <whodatbio@gmail.com>
2022-09-28 11:40:01 -0400

    char cust js

--- src/components/pages/CharCust.js
+++ src/components/pages/CharCust.js
@@ -1,49 +1,11 @@
-import MoodyGamer from '../../images/moody-gamer.png'
-import CampaignManager from '../../images/campaign-manager.PNG'
-import CodeQuiz from '../../images/code-quiz.png'
-import NoteTaker from '../../images/note-taker.PNG'
-import WorkDay from '../../images/work-day.png'
-import WeatherDash from '../../images/weather-dash.PNG'
-
-const Characters = [
-  {
-    id: 1,
-    name: "Moody Gamer",
-    image: MoodyGamer
-  },
-  {
-    id: 2,
-    name: "Campaign Manager",
-    image: CampaignManager
-  },
-  {
-    id: 3,
-    name: "Note Taker",
-    image: NoteTaker
-  },
-  {
-    id: 4,
-    name: "Code Quiz",
-    image: CodeQuiz
-  },
-  {
-    id: 5,
-    name: "Weather Dashboard",
-    image: WeatherDash
-  },
-  {
-    id: 6,
-    name: "Work Day Scheduler",
-    image: WorkDay
-  },
-]
-
 const CharCust = () => {
 
   return (
     <div>
       <h1 className='text-5xl mb-12 text-yellow-100 text-center'>Character Custimazation</h1>
-      
+      <form className="text-center">
+      <input className= 'text-center' placeholder="enter name"></input>
+      </form>
     </div>
   );
 };
