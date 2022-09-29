151b191d4cfa2c229a2ce8243f30d882ec3862e9
Eric <whodatbio@gmail.com>
2022-09-29 10:17:45 -0400

    saving

--- client/src/components/DungeonContainer.js
+++ client/src/components/DungeonContainer.js
@@ -31,15 +31,14 @@ function DungeonContainer() {
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <div className='mx-auto bg-emerald-600 '>
-      <div className='flex flex-col lg:flex-row justify-between p-12 bg-emerald-500 drop-shadow-xl'>
+    <div className='mx-auto bg-gray-800 h-screen '>
+      <div className='flex flex-col lg:flex-row justify-between p-12 bg-gray-900 drop-shadow-xl'>
         <Header />
         <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
       </div>
       <div className='m-16'>
         {renderPage()}
       </div>
-      <Footer />
     </div>
   );
 }
--- client/src/components/pages/AboutMe.js
+++ client/src/components/pages/AboutMe.js
@@ -3,17 +3,17 @@ import React from 'react';
 export default function AboutMe() {
   return (
     <div>
-      <h1 className='text-5xl mb-12 text-yellow-100 text-center'>About Us</h1>
-      <p className='text-2xl text-purple-100 ml-4 leading-loose'>Voravich Silapachairueng:</p>
-        <li className='text-blue-100'><a href='https://github.com/voravichs'>His Github</a></li>
-        <li className='text-blue-100'><a href='https://voravichs.github.io/voravich-react-portfolio/'>His Portfolio</a></li>
-      <p className='text-2xl text-purple-100 ml-4 leading-loose'>Wyatt Willner:</p>
-        <li className='text-blue-100'><a href='https://github.com/wyattwillner55'>His Github</a></li>
-      <p className='text-2xl text-purple-100 ml-4 leading-loose'>Ida Kukimiya:</p>
-        <li className='text-blue-100'><a href='https://github.com/idakukimiya'>Her Github</a></li>
-        <li className='text-blue-100'><a href='https://idakukimiya.github.io/Ida_REACTportfolio/ '>Her Portfolio</a></li>
-      <p className='text-2xl text-purple-100 ml-4 leading-loose'>Eric Courter:</p>
-        <li className='text-blue-100'><a href='https://github.com/Amob7'>His Github</a></li>
+      <h1 className='text-5xl mb-12 text-blue-300 text-center'>About Us</h1>
+      <p className='text-2xl text-teal-200 ml-4 leading-loose'>Voravich Silapachairueng:</p>
+        <li className='text-teal-100'><a href='https://github.com/voravichs'>His Github</a></li>
+        <li className='text-teal-100'><a href='https://voravichs.github.io/voravich-react-portfolio/'>His Portfolio</a></li>
+      <p className='text-2xl text-teal-200 ml-4 leading-loose'>Wyatt Willner:</p>
+        <li className='text-teal-100'><a href='https://github.com/wyattwillner55'>His Github</a></li>
+      <p className='text-2xl text-teal-200 ml-4 leading-loose'>Ida Kukimiya:</p>
+        <li className='text-teal-100'><a href='https://github.com/idakukimiya'>Her Github</a></li>
+        <li className='text-teal-100'><a href='https://idakukimiya.github.io/Ida_REACTportfolio/ '>Her Portfolio</a></li>
+      <p className='text-2xl text-teal-200 ml-4 leading-loose'>Eric Courter:</p>
+        <li className='text-teal-100'><a href='https://github.com/Amob7'>His Github</a></li>
     </div>
   );
 }
