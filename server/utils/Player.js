e95674b5e87ccc228ef41702635f612df1fd680d
wyattwillner55 <109093691+wyattwillner55@users.noreply.github.com>
2022-09-29 10:39:50 -0400

    Add files via upload

--- server/utils/Player.js
+++ server/utils/Player.js
@@ -7,32 +7,42 @@ class Player {
         this.magic = magic;
         this.defense = defense;
         this.mDefense = mDefense;
+        this.dead = false;
     }
     //attack functions
     physicalAttack(target) {
-        let rng = Math.floor(math.random() * 11) 
+        let rng = Math.floor(math.random() * 11); 
         if (rng < 2) return 0;
         else if (2 < rng && rng < 10) {
-            return (this.attack - target.defense) - this.currentHealth;
+            target.currentHealth = target.currentHealth - (this.attack - target.defense);
+            return  (this.attack - target.defense);
         }
         if (rng == 10) {
-            return (this.attack - target.defense)*2 - this.currentHealth;
+            target.currentHealth = target.currentHealth - (1.5 * this.attack - target.defense);
+            return  (this.attack - target.defense);
         }
     }
     magicAttack(target) {
         let rng = Math.floor(math.random() * 11) 
         if (rng < 2) return 0;
         else if (2 < rng && rng < 10) {
-            return (this.magic - target.mDefense) - this.currentHealth;
+            target.currentHealth = target.currentHealth - (this.magic - target.mDefense);
+            return  (this.magic - target.mDefense);
         }
         if (rng == 10) {
-            return (this.magic - target.mDefense)*2 - this.currentHealth;
+            target.currentHealth = target.currentHealth - (1.5 * this.magic - target.mDefense);
+            return  (1.5 * this.magic - target.mDefense);
         }
     }
     //heal function
     heal(amount) {
         this.currentHealth = this.currentHealth + amount;
         if (this.currentHealth > this.maxHealth) this.currentHealth = this.maxHealth;
+    }
+    //checks if a character is dead
+    isDead() {
+        if (this.currentHealth > 0) return false;
+        else if (this.currentHealth <= 0) return true;
     }
     //level up function
     levelup() {
