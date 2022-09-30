01478764fc6ebf066dfcc15aa565bbcf74a4a3f7
VoravichS <vsilapac@gmail.com>
2022-09-30 13:59:11 -0400

    gradients to character creation box

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../images/logo.png'
 
 
 export default function Header() {
   return (
     <div className='text-blue-300'>
-      <h1 className='text-6xl'>Mern Dungeon</h1>
+      <Link
+        to="/">
+        <img className='w-2/3' src={Logo} alt='pixel battle logo'></img>
+      </Link>
     </div>
   );
 }
